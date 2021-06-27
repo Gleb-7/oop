69397d9f4209e12958f4f1536616efd9cabe2ea9
Глеб Синицын <glebptica87@gmail.com>
2021-06-27 16:42:53 +0300

    проверка домашнего здания

--- oop/oop.cpp
+++ oop/oop.cpp
@@ -1,20 +1,48 @@
-﻿// oop.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <math.h>
+#include <cstdint>
+using namespace std;
+class Power
+{
+private:
+	int m_a = 5;
+	int m_b = 6;
+public:
+	void seta(int a) { m_a = a; }
+	void setb(int b) { m_b = b; }
+	void calculate()
+	{
+		cout << pow(m_a, m_b) << endl;
+	}
+};
 
-#include <iostream>
+class RGBA
+{
+private:
+	uint8_t m_red = 0;
+	uint8_t m_green = 0;
+	uint8_t m_blue = 0;
+	uint8_t m_alpha;
+public:
+	RGBA(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha = 255) :m_red(red), m_green(green), m_blue(blue), m_alpha(alpha)
+	{
+		cout << "constructor" << endl;
+		cout << "red" << m_alpha;
+	}
+	void print()
+	{
+		cout << "red" << static_cast<uint16_t>(m_alpha);
+	}
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Power power;
+	power.calculate();
+
+	RGBA rgba(0, 0, 0);
+	rgba.print();
+
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
